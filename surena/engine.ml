1511dfe4b701099ae3aa9b870170c5fed6ed8970
Elie Michel <elie.michel@ens.fr>
2014-03-28 23:07:36 +0100

    Création d'un dossier tout beau tout propre pour le refactor

---
+++ surena/engine.ml
@@ -0,0 +1,17 @@
+(*
+	Engine exécute la simulation du systeme et définit des types et procédures
+	permettant sa manipulation.
+*)
+
+
+
+
+let step rules positions =
+	(**
+		`step rules positions`
+		met à jour le vecteur de positions à partir des règles données.
+	*)
+	()
+
+
+
---
+++ surena/invert.ml
@@ -0,0 +1,17 @@
+(*
+*)
+
+
+let main filename =
+	()
+
+
+let () = Arg.parse
+	[]
+	main
+	"Usage : invert data.\n\
+	Tries to recover the parameters given to a simulation from the position\n\
+	outut. These data are given as first argument.\n\
+	The available options are :"
+
+
