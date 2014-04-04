c80c60490fcf758bd2b2f07005214f58038fc549
Émile Enguehard <zerbugug@gmail.com>
2014-04-04 14:52:48 +0200

    aaaaa

--- surena/display.ml
+++ surena/display.ml
@@ -48,10 +48,10 @@ let rules cb cm ca cl rb rm ra rl ab am aa al ib =
 
 let rules =
 	let rules,(cb,_,_,_),(_,rm,ra,rl),_,_ = rules
-		0.01 1. 0.5 100.
+		0.02 1. 0.5 100.
 		10. 1. 2. 20.
 		0.5 1. 0.5 100.
-		0.5 in
+		0.45 in
 	cb.(0) <- 0.1;
 	for i = 1 to n - 1 do
 		rl.(i).(0) <- 100.;
@@ -66,7 +66,9 @@ let () = boids.(0) <- { boids.(0) with color = Graphics.blue }
 
 let main arg =
 	Random.self_init ();
-	open_graph " 600x600";
+	let s = " " ^ (string_of_int Engine.capxi) ^ "x"
+		^ (string_of_int Engine.capyi) in
+	open_graph s;
 	set_window_title "Boids";
 	auto_synchronize false;
 	while not (key_pressed () && read_key () = 'q') do
