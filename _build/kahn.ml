81fc9ce3affc894b1f7847cdede29ebf43b06059
Oth,Saf <othmane.safsafi@ens.fr>
2014-04-10 22:24:27 +0200

    azfaef
    afazraz

--- _build/kahn.ml
+++ _build/kahn.ml
@@ -94,40 +94,49 @@ module Th: S = struct
 end
 
 module Proc: S = struct 
-	type canal = {file: Unix.file_descr; l: int list ref}
 	type 'a process = (unit -> 'a)
-	type 'a in_port = canal
-	type 'a out_port = canal
+	type 'a in_port = in_channel
+	type 'a out_port = out_channel
 	
-	let new_channel () = let (a,b) = pipe () in  ({file = a; l = ref []},{file = b;l = ref []}) 
-	let put_proc v fd () = begin let a = Marshal.to_string v [Marshal.No_sharing] in 
-	ignore (Unix.write  fd.file a 0 ((String.length a)-1)); Unix.close fd.file;
-					fd.l := (String.length a)::!(fd.l) end
+	let new_channel () = 
+	let (a,b) = pipe () in  
+	let c  = in_channel_of_descr a in 
+	let d = out_channel_of_descr b in 
+	(c,d)
+	
+	
+	let put_proc v fd () = 
+	Marshal.to_channel fd v  [No_sharing];;
+	
 	let put v fd = put_proc v fd ;;
 	
 	
-	let get fd () = let buff = String.create (List.hd (!(fd.l))) in 
-	ignore (Unix.read fd.file buff 0 ((String.length buff)-1)); Unix.close fd.file;
-	fd.l := List.tl (!(fd.l));  
-	let a =  Marshal.from_string buff 0 in a ;;
+	let get_aux fd () =  
+	let a = Marshal.from_channel fd in a;;
+	
+	
+	
+	let get fd = get_aux fd
 	
 
 		
 	
-	let rec aux_doco l sortie entree () = match l with 
-	| [] -> ignore (Unix.read sortie "" 0 0)
+	let rec aux_doco l () = match l with 
+	| [] -> ignore(exit 0); 
 	| h::q -> match Unix.fork () with 
-			| 0 -> h (); ignore (Unix.write entree "" 0 0); Unix.close entree; ()
-			| _ -> aux_doco q sortie entree ()
-	let doco (l : unit process list) = let (sortie, entree) = pipe () in 
-			aux_doco l sortie entree
+			| 0 -> aux_doco q  ()
+			| _ -> h ();  ignore(Unix.wait ()); ignore (exit 0)
+			
+	let doco (l : unit process list) = 
+	aux_doco l 
 		
 	let return a = (fun () -> a)
 	
-	let bind e e' () = let (sortie, entree) = pipe () in let v = e () in e' v ()	
-		(*match Unix.fork() with 
-			| 0 -> e' v (); ignore (Unix.write entree "" 0 0); Unix.close entree
-			| _ -> ignore (Unix.read sortie "" 0 0);*)
+	
+	let bind_aux e e' ()  =  let v = e () in e' v ()
+	let bind e e' = bind_aux e e' 
+	
+	
 			
 	let run e = e ()
 	
