f89f2c03b09c09eb129bca8c4b7ebf0aae0aba7b
Oth,Saf <othmane.safsafi@ens.fr>
2014-04-10 22:44:47 +0200

    PUTAIN DE MERDE

--- surena/invert.ml
+++ surena/invert.ml
@@ -3,6 +3,47 @@
 	les paramètres qui l'ont conduit.
 *)
 
+open Engine
+
+let _ = Random.init (Unix.time ())
+
+let sigmoida alpha lambda x =
+	let d = x -. lambda in
+	let e = exp (alpha *. d) in
+	let e1 = 1. +. e in
+	-. (d *. e) /. (e1 *. e1)
+	
+let sigmoidl alpha lambda x =
+	let e = exp (alpha *. (x -. lambda)) in
+	let e1 = 1. +. e in
+	(alpha *. e) /. (e1 *. e1)
+	
+let sigmoidx alpha lambda x =
+	let e = exp (alpha *. (x -. lambda)) in
+	let e1 = 1. +. e in
+	-. (alpha *. e) /. (e1 *. e1)
+	
+let dsigmoid alpha lambda x =
+	let d = x -. lambda in
+	let e = exp (alpha *. d) in
+	let e1 = 1. +. e in
+	let f = e /. (e1 *. e1) in
+	let fa = alpha *. f in
+	(-. d *. f), fa, (-. fa)
+
+let random_float a b = Random.float (b -. a) +. a
+
+(*
+	Résolution dans le cas où les paramètres sont uniformes.
+	On suppose alpha constant.
+	rm ne sert à rien, les paramètres sont :
+		cb cm cl
+		rb rl
+		ab am al
+		ib
+	tous de type float.
+*)
+
 
 
 let main filename =
